442a9907bab187e97af584eba3a8745cefb70ce0
RussellJi <17361564789@163.com>
2021-05-13 12:09:14 +0800

    log

---
+++ liao/exceptions/src/com/logging/Main.java
@@ -0,0 +1,11 @@
+package src.com.logging;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Log log = LogFactory.getLog(Main.class);
+        log.info("start...");
+        log.warn("end.");
+    }
+}
