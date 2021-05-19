dcca57f02063fcc52f6014fe70dbb9bdf42fbcbb
RussellJi <17361564789@163.com>
2021-05-19 16:02:18 +0800

    list exercise

---
+++ liao/Collections/Collection/List/Exercise/ListExercise.java
@@ -0,0 +1,58 @@
+package Collections.Collection.List.Exercise;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class ListExercise {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        List list1 = new ArrayList();
+        List list2 = new LinkedList();
+        List list3 = new Vector();
+        list1.add(new Book("骆驼祥子",50,"老舍"));
+        list1.add(new Book("西游记",49,"吴承恩"));
+        list1.add(new Book("红楼梦",66,"曹雪芹"));
+
+        System.out.println(list1);
+
+        // 冒泡排序
+        for(int i = 0; i<list1.size()-1; i++){
+            for(int j = 0; j<list1.size()-1-i; j++){
+                int p1 = ((Book) list1.get(j)).getPrice();
+                int p2 = ((Book) list1.get(j+1)).getPrice();
+                if(p1 > p2){
+                    Object temp = list1.get(j+1);
+                    list1.set(j+1,list1.get(j));
+                    list1.set(j, temp);
+                }
+            }
+        }
+        System.out.println("list1:"+list1);
+
+        list2.add(new Book("骆驼祥子",50,"老舍"));
+        list2.add(new Book("西游记",49,"吴承恩"));
+        list2.add(new Book("红楼梦",66,"曹雪芹"));
+
+        System.out.println(list2);
+
+        // 冒泡排序
+        for(int i = 0; i<list2.size()-1; i++){
+            for(int j = 0; j<list2.size()-1-i; j++){
+                int p1 = ((Book) list2.get(j)).getPrice();
+                int p2 = ((Book) list2.get(j+1)).getPrice();
+                if(p1 > p2){
+                    Object temp = list2.get(j+1);
+                    list2.set(j+1,list2.get(j));
+                    list2.set(j, temp);
+                }
+            }
+        }
+        System.out.println("list2:"+list2);
+
+    }
+
+
+
+}
