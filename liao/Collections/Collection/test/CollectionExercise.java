226dfc66ca3bc819e5f1c6580e68080e696c2fa9
RussellJi <17361564789@163.com>
2021-05-19 10:21:43 +0800

    collection exercise

---
+++ liao/Collections/Collection/test/CollectionExercise.java
@@ -0,0 +1,23 @@
+package Collections.Collection.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionExercise {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(new Dog("a",1));
+        list.add(new Dog("b",2));
+        System.out.println("list:"+list);
+        Iterator it = list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        for(Object o : list){
+            System.out.println(o);
+        }
+    }
+
+}
