68d183ce26a11a23306545e0567ab162ad715517
RussellJi <17361564789@163.com>
2021-06-06 16:27:13 +0800

    getname

--- liao/Net_HSP/udp/UDP_Receiver.java
+++ liao/Net_HSP/udp/UDP_Receiver.java
@@ -8,7 +8,7 @@ import java.net.InetAddress;
 
 public class UDP_Receiver {
     public static void main(String[] args) throws IOException {
-        // 监听8888端口
+        // 创建接收端，监听8888端口
         DatagramSocket datagramSocket = new DatagramSocket(8888);
         byte[] bytes = new byte[1024];
         DatagramPacket datagramPacket = new DatagramPacket(bytes,1024);
@@ -17,6 +17,7 @@ public class UDP_Receiver {
         System.out.println(datagramPacket.getLength());
         String recMsg = new String(datagramPacket.getData(),0,datagramPacket.getLength());
         System.out.println("接收到:"+recMsg);
+        // 将要发送的数据转为byte
         String sendDataStr = "ACK:"+recMsg;
         byte[] sendData = sendDataStr.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(sendData,0,sendData.length,InetAddress.getByName("192.168.3.49"),9999);
